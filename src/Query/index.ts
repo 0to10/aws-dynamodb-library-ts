010946e2eb904c1aeeb721d595b708fb11d3b5a1
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2025-01-25 10:04:24 +0100

    Added index.ts files.
    Signed-off-by: 0TO10 <121996729+0to10-dev@users.noreply.github.com>

---
+++ src/Query/index.ts
@@ -0,0 +1,3 @@
+'use strict';
+
+export {QueryCommandOutputMapper} from './QueryCommandOutputMapper';
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+import {Collection} from './Collection';
+
+export {
+    Collection,
+};
